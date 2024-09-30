030d0b8b0ed7ee5a7a8ca2117fb6f51ecd7e88ff
madvin <madvin@abv.bg>
2024-09-30 16:43:05 +0300

    dff

--- 05.JS-Advanced-DOM-Events-Exercise-Resources/04. Fill-Dropdown/dropdown.js
+++ 05.JS-Advanced-DOM-Events-Exercise-Resources/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,20 @@
 function addItem() {
-    console.log('TODO:...');
+
+    const inputTextEl = document.querySelector('#newItemText');
+    const inputValueEl = document.querySelector('#newItemValue');
+
+    const newOption = document.querySelector('#menu');
+
+    const newItem = document.createElement('option');
+    newItem.textContent = inputTextEl.value;
+    newItem.setAttribute('value', inputValueEl.value);
+
+    newOption.appendChild(newItem);
+
+
+    inputTextEl.value = '';
+    inputValueEl.value = '';
+
+    inputTextEl.focus();
+
 }
--- 05.JS-Advanced-DOM-Events-Exercise-Resources/05. Encode-and-Decode-Messages/app.js
+++ 05.JS-Advanced-DOM-Events-Exercise-Resources/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,37 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    
+    const inputEl = document.querySelector('#main > div:nth-child(1) textarea');
+    const outputEl = document.querySelector('#main > div:nth-child(2) textarea');
+
+    const encodeButtonEl = document.querySelector('#main > div:nth-child(1) button');
+    const decodeButtonEl = document.querySelector('#main > div:nth-child(2) button');
+
+    let codedArr = [];
+    let encodedArr = [];
+
+    encodeButtonEl.addEventListener('click', encode); 
+   
+    function encode() {
+        let arr = inputEl.value.split('');
+        arr.forEach((el) => codedArr.push(el.charCodeAt(0) + 1));
+        outputEl.value = codedArr.forEach(el => encodedArr.push(String.fromCharCode(el)));
+        outputEl.value = encodedArr.join('')
+        inputEl.value = '';
+        codedArr = [];
+        encodedArr = [];
+        arr = [];
+    }
+
+    decodeButtonEl.addEventListener('click', decode); 
+
+    function decode() {
+        let tempArr = outputEl.value.split('');
+        tempArr.forEach((el) => codedArr.push((Number(el.charCodeAt(0)) - 1)));
+        let arr = [];
+        codedArr.forEach((el) => arr.push(String.fromCharCode(el)));
+        outputEl.value = arr.join('');
+        arr = [];
+        encodedArr = [];
+        codedArr = [];
+    }
 }
