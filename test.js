498378a1775b7163d55796a15dcefb98735539aa
madvin <madvin@abv.bg>
2024-10-10 09:22:00 +0300

    dd

--- test.js
+++
@@ -1,9 +0,0 @@
-const person = {
-    name: "Kolyo",
-    age: 22,
-    height: 178
-}
-
-Object.defineProperty(person, 'name', { configurable: false, writable: true });
-
-console.log(person.name);
